0e30bab3a0476fe2c178c5f522fd5eea60d2e821
florah001 <florah001@yahoo.co.uk>
2018-10-07 11:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 function shout(string) {
-  return string.toUpperCase("YES INDEED");
+  return string.toUpperCase("HELLO");
 }
 function whisper(string) {
   return string.toLowerCase("hello");
 } 
-function sayHiToGrandma(string) {
-  return string.toUpperCase("YES INDEED");
-}
-function sayHiToGrandma(string) {
-  return string.toLowerCase("I can't hear you!");
-}
-function sayHiToGrandma(string) {
-  console.log("I can't hear you!");
-}
-function sayHiToGrandma() {
- console.log("hello");
-}
-function sayHiToGrandma(string) {
-  console.log("YES INDEED");
-}
-function sayHiToGrandma(string) {
-  return string.toLowerCase("I love you, too.");
-}
-function sayHiToGrandma(string) {
-  if (sayHiToGrandma(string)=== hello);
-}
 function logShout(string) {
   console.log("HELLO");
 }
 function logWhisper(string) {
   console.log('hello');
 }
-
-function sayHiToGrandma(string) {
-if (string === "hello".toLowerCase()) {
-  console.log("I can't hear you!");
-  
-} else if (string === string.toUpperCase()){
-  console.log("YES INDEED")
-  
-} 
-}
-if (sayHiToGrandma === ("i love you, Grandma".toLowerCase)) {
-console.log("i can't hear you")
+ function sayHiToGrandma(string) {
+    console.log("hello")
+    return string.toLowerCase("i can\'t hear you")
 }
 
-/*else if ("I love you, Grandma" === string.toLowerCase()) {
-  console.log("I love you, too.")  
-}*/
 
+
+
+
+
+
+
+/*
+if (sayHiToGrandma(string) === string.toLowerCase')
+return string.toLowerCase("i can\'t hear you")
+
+/*
+sayHiToGrandma(string)
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+/*
+
+ function sayHiToGrandma(string) {
+  if(string === string.toLowerCase()) {
+    return string.toLowerCase("i can\'t hear you")
+      } else if (string === string.toUpperCase()) {
+         return string.toUpperCase("YES INDEED")
+          }
+        }
+*/
 
 
 
